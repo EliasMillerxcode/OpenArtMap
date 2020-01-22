8d7541cf6f7c0c209aabbfa37160c11b93243044
Mark Stamer <markstamer@gmail.com>
2020-01-22 17:50:01 +0100

    Adds toggle red ball example

--- OpenArtMap/ContentView.swift
+++ OpenArtMap/ContentView.swift
@@ -1,8 +1,34 @@
 import SwiftUI
 
-struct ContentView: View {
+struct Ball: View {
+
+    let color: Color
+
     var body: some View {
-        Text("Hello, Elias and Mark!")
+        color
+            .clipShape(Circle())
+            .frame(width: 200, height: 200, alignment: .center)
+    }
+}
+
+struct ContentView: View {
+
+    @State var redBallIsShown: Bool = true
+
+    var body: some View {
+        VStack {
+            Toggle(isOn: $redBallIsShown.animation()) {
+                Text("Toggle red ball.")
+            }.padding()
+            Ball(color: .yellow)
+            if redBallIsShown {
+                Ball(color: .red)
+            }
+            ZStack {
+                Ball(color: .blue)
+                Text("Hello, Elias and Mark!")
+            }.padding(.all, 24)
+        }
     }
 }
 
