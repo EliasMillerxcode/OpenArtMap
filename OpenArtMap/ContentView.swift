24af0a084e7f50ea3677d7c27384cd19f104a026
Elias Miller <elias.miller>
2020-01-22 17:06:24 +0100

    Changes text name

--- OpenArtMap/ContentView.swift
+++ OpenArtMap/ContentView.swift
@@ -1,16 +1,8 @@
-//
-//  ContentView.swift
-//  OpenArtMap
-//
-//  Created by Claudia Miller on 22.01.20.
-//  Copyright © 2020 Miller Prodactions. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello, Elias!")
     }
 }
 
